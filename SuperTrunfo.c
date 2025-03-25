3fff33122578941043445a79081c18679e534b50
programacaogit-hub <lhsfestudante@gmail.com>
2025-03-25 02:55:12 +0000

    Atualizando codigo para nivel mestre

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -10,15 +10,18 @@ int main(){
     char codigo1[3] = "L01";
     char cidade1[20] = "Ancre";          
     int populacao1 = 200000, pontos1 = 30;
-    float area1 = 5000.00, pib1 = 6.000;
+    float area1 = 5000.00, pib1 = 6.000, densidadep1 = 40, pibpercap1 = 0.03;
+    double superpoder1 = 211070.03;
 
     char estado2;
     char codigo2[3];
     char cidade2[20];          
     int populacao2, pontos2;
-    float area2, pib2;
+    float area2, pib2, densidadep2, pibpercap2;
+    double superpoder2;
 
     //Entrada de dados.
+    printf("Entre com os dados da primeira carta: \n");
     printf("Digite uma letra de A a H para representar o Estado: \n");
     scanf("%c", &estado1);
 
@@ -40,6 +43,7 @@ int main(){
     printf("Digite quantidade de pontos turísticos da cidade: \n");
     scanf("%d", &pontos1);
 
+    printf("Entre com os dados da segunda carta: \n");
     printf("Digite uma letra de A a H para representar o Estado: \n");
     scanf("%c", &estado2);
 
@@ -61,6 +65,19 @@ int main(){
     printf("Digite quantidade de pontos turísticos da cidade: \n");
     scanf("%d", &pontos2);
 
+    //Cálculo da densidade populacinal e do pib per capita.
+    densidadep1 = (float) pibpercap1 / area1;
+    pibpercap1 = (float) pib1 / populacao1;
+
+    densidadep2 = (float) pibpercap2 / area2;
+    pibpercap2 = (float) pib2 / populacao2;
+
+    //Cálculo do super poder
+    superpoder1 = (double) populacao1 + pontos1 + area1 + pib1 + densidadep1 + pibpercap1;
+
+    superpoder2 = (double) populacao2 + pontos2 + area2 + pib2 +densidadep2 + pibpercap2;
+
+    printf("Carta1 \n");
     printf("Estado: %c \n", estado1);
     printf("Código: %s \n", codigo1);
     printf("Nome da Cidade: %s \n", cidade1);
@@ -68,7 +85,11 @@ int main(){
     printf("Área: %.2f km² \n", area1);
     printf("PIB: %.2f \n", pib1);
     printf("Números de Ponto Turísticos: %d \n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km² \n", densidadep1);
+    printf("Pib per Capita: %.2f reais \n", pibpercap1);
+    printf("Super Poder: %.2f \n", superpoder1);
 
+    printf("Carta2 \n");
     printf("Estado: %c \n", estado2);
     printf("Código: %s \n", codigo2);
     printf("Nome da Cidade: %s \n", cidade2);
@@ -76,6 +97,9 @@ int main(){
     printf("Área: %.2f km² \n", area2);
     printf("PIB: %.2f \n", pib2);
     printf("Números de Ponto Turísticos: %d \n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km² \n", densidadep2);
+    printf("Pib per Capita: %.2f reais \n", pibpercap2);
+    printf("Super Poder: %.2f \n", superpoder2);
 
     return 0;
 
